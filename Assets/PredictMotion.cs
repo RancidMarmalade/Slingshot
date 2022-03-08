2b78b93e673800ed18565e4246aec9a916c60e6a
davidala1 <78424061+davidala1@users.noreply.github.com>
2022-03-08 12:29:14 +0100

    upgraded visuals og seje ting

--- Assets/PredictMotion.cs
+++ Assets/PredictMotion.cs
@@ -12,6 +12,9 @@ public class PredictMotion : MonoBehaviour
     private float StepLength;
     public float G;
     public float fps;
+
+    [SerializeField]
+    private float D;
     // Update is called once per frame
     private void Start()
     {
@@ -32,6 +35,7 @@ public class PredictMotion : MonoBehaviour
     }
     private void OnDrawGizmos()
     {
+        D = 100000f;
  
         GameObject[] attractors = GameObject.FindGameObjectsWithTag("Attract");
         CurrentPos = transform.position;
@@ -46,16 +50,26 @@ public class PredictMotion : MonoBehaviour
                 {
                     Vector2 direction = (new Vector2(Attractor.transform.position.x, Attractor.transform.position.y) - CurrentPos);
                     float dist = direction.magnitude;
+                    
                     float ForceMagnitude = (Attractor.GetComponent<Rigidbody2D>().mass - GetComponent<Rigidbody2D>().mass) / Mathf.Pow(dist, 2) * G;
                     V += ForceMagnitude * direction.normalized;
                    
-                    
+                    if(D >= dist)
+                    {
+                        D = dist;
+                        
+                    }
                 }
             }
             StepLength = (V.magnitude * (Time.fixedDeltaTime / fps)) / GetComponent<Rigidbody2D>().mass;
             CurrentPos += V.normalized * StepLength;
             Gizmos.DrawLine(CurrentPos, CurrentPos += V.normalized * StepLength);
             
+            if(D <= 0.9f)
+            {
+                break;
+            }
+           
         }
     }
 }
